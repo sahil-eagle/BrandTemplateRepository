53d54b4bb5f6a6575a5e2e837368ee4442d4399c
sahil2343 <sahil.pradhan@mountblue.tech>
2021-06-03 18:28:29 +0530

    added comments and logic for different methods

--- customer_registration/main.go
+++ customer_registration/main.go
@@ -13,7 +13,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
-type Item struct {
+type SignUpObject struct {
 	pk             string
 	sk             string
 	CustomerEmail  string
@@ -37,57 +37,67 @@ func postHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyR
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
+
+	// Starting a DynamoDB Session
 	svc := dynamodb.New(sess)
-	var item Item
-	err := json.Unmarshal([]byte(request.Body), &item)
-	if err != nil {
-		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 404}, nil
-	}
 
-	UserPrefix := "User#"
-	newItem := ItemResponse{
-		pk:             UserPrefix + item.CustomerEmail,
-		sk:             item.sk,
-		CustomerEmail:  item.CustomerEmail,
-		CustomerMobile: item.CustomerMobile,
-		CustomerName:   item.CustomerName,
-		Password:       item.Password,
-		IsVerified:     item.IsVerified,
-	}
+	// Checking if method is signup
+	if request.Body.methods == "signup" {
+		var item SignUpObject
+		err := json.Unmarshal([]byte(request.Body), &item) // converting the request body to json type(byte data) and then into struct object, assigning to item
+		if err != nil {
+			return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 404}, nil
+		}
 
-	response, err := json.Marshal(&newItem)
-	if err != nil {
-		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 404}, nil
-	}
-	tableName := "BrandTemplateTable"
+		UserPrefix := "User#"
+		newUser := ItemResponse{
+			pk:             UserPrefix + item.CustomerEmail,
+			sk:             item.sk,
+			CustomerEmail:  item.CustomerEmail,
+			CustomerMobile: item.CustomerMobile,
+			CustomerName:   item.CustomerName,
+			Password:       item.Password,
+			IsVerified:     item.IsVerified,
+		}
 
-	av, err4 := dynamodbattribute.MarshalMap(newItem)
-	if err4 != nil {
-		log.Fatalf("Got error marshalling new item: %s", err4)
-	}
+		_, err = json.Marshal(&newUser) // Convert struct data into JSON
+		if err != nil {
+			return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 404}, nil //Error if not found
+		}
+		tableName := "BrandTemplateTable"
 
-	result, err := svc.GetItem(&dynamodb.GetItemInput{
-		TableName: aws.String(tableName),
-		Key: map[string]*dynamodb.AttributeValue{
-			"pk": {
-				S: aws.String(newItem.pk),
+		data, err2 := dynamodbattribute.MarshalMap(newUser)
+		if err2 != nil {
+			log.Fatalf("Got error marshalling new item: %s", err2)
+		}
+
+		// DynamoDB getItem method for checking user existance
+		result, err := svc.GetItem(&dynamodb.GetItemInput{
+			TableName: aws.String(tableName),
+			Key: map[string]*dynamodb.AttributeValue{
+				"pk": {
+					S: aws.String(newUser.pk),
+				},
 			},
-		},
-	})
+		})
 
-	if result != nil {
-		return events.APIGatewayProxyResponse{Body: "User with this email already exist", StatusCode: 400}, nil
-	}
+		// Checking if User with given email exist
+		if result != nil {
+			return events.APIGatewayProxyResponse{Body: "User with this email already exist", StatusCode: 400}, nil
+		}
 
-	input := &dynamodb.PutItemInput{
-		Item:      av,
-		TableName: aws.String(tableName),
+		input := &dynamodb.PutItemInput{
+			Item:      data,
+			TableName: aws.String(tableName),
+		}
+		_, err = svc.PutItem(input)
+		if err != nil {
+			log.Fatalf("Got error calling PutItem: %s", err)
+		}
+
+		// Returning the response object that has been written to the Database
 	}
-	_, err = svc.PutItem(input)
-	if err != nil {
-		log.Fatalf("Got error calling PutItem: %s", err)
-	}
-	return events.APIGatewayProxyResponse{Body: string(response), StatusCode: 200}, nil
+	return events.APIGatewayProxyResponse{Body: string("Data written"), StatusCode: 200}, nil
 
 }
 
